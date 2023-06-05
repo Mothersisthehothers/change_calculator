8278ed364eee69a3df0f2cc95ade08b1b930d26a
Mothersisthehothers <ptc.05perezp@myptc.edu>
2023-06-05 13:36:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@
 */
 
 // Global variables
-let cashBox = document.getElementById(cash);
-let billBox = document.getElementById(bill);
-let changeBox = document.getElementById(change);
+let cashBox = document.getElementById("cash");
+let billBox = document.getElementById("bill");
+let changeBox = document.getElementById("change");
 
 // Event handlers to be run when the cash or bill value changes
 cashBox.addEventListener("change", runTheRegister);
@@ -38,10 +38,15 @@ function runTheRegister() {
    zeroTheRegister();
    
    let changeValue = cashBox.value - billBox.value;  // calculate the change 
+   try {
+      if (!(changeValue >= 0))
+         throw "Cash amount doesn't cover the bill"; 
+         changeBox.value = formatCurrency(changeValue); // format the change as currency
+         calcChange(changeValue); // Determine the units of currency needed for the change
+   }  catch(error) {
+         document.getElementById("warning").innerHTML = error;
+   }
    
-   changeBox.value = formatCurrency(changeValue); // format the change as currency
-   
-   calcChange(changeValue); // Determine the units of currency needed for the change
 }
 
 // Function to calculate the change by each unit of currency
@@ -59,7 +64,7 @@ function calcChange(changeValue) {
    // Determine the number of $5 bills
    let bill5Amt = determineCoin(changeValue, 5);
    document.getElementById("bill5").innerHTML = bill5Amt;
-   changeValue -=  bill5Amt*3;  
+   changeValue -=  bill5Amt*5;  
    
    // Determine the number of $1 bills
    let bill1Amt = determineCoin(changeValue, 1);
